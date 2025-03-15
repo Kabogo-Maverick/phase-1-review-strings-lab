b5498ef96ba695fd9a8e3a37313bcd23375fb99a
mkay <mkay@gmail.com>
2025-03-15 21:58:32 +0300

    Added welcome message feature

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your code in this file!
+const currentUser = 'Double Dragon';
+console.log(currentUser)
+
+const welcomeMessage = `Welcome to Kiambu, ${currentUser}`;
+console.log(welcomeMessage)
+
+console.log(typeof currentUser)
+
+console.log(currentUser.length)
+
+const bigwelcomemessage = welcomeMessage.toUpperCase()
+console.log(bigwelcomemessage)
+
+console.log(currentUser.slice(7))
+
+console.log(currentUser.slice(0, 6))
+
+const shortgreetings = `welcome, ${currentUser.slice(0, 6)} to Nairobi`
+console.log(shortgreetings)
